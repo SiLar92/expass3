fe5fe3e9d8ead7e684cfe1b4e1372706ca1e69c1
Sindre Larsen <MindreDisseHandling>
2021-09-13 22:19:02 +0200

    Finished CRUD / Experiment 1

--- src/main/java/experiment1/CRUD.java
+++ src/main/java/experiment1/CRUD.java
@@ -6,7 +6,10 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.*;
 import org.bson.Document;
+
+import java.util.Arrays;
 
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.*;
@@ -15,34 +18,38 @@ import static java.util.Collections.singletonList;
 
 public class CRUD {
     public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
         MongoDatabase db = mongoClient.getDatabase("expass3-insert");
 
         MongoCollection<Document> collection = db.getCollection("CRUD");
 
 
         // Insert
-        for (int i = 0; i < 10; i++) {
-            insert(collection, i);
-        }
+        insert(collection, 0);
 
-        // Print all from query: SELECT * FROM collection
+        System.out.println("\n-------- After Single insert --------");
 
         printAll(collection);
-        System.out.println("\n-------- Before update --------");
+
+
+
+        System.out.println("\n-------- After Bulk insert --------");
+        bulk(collection);
+
+
+        printAll(collection);
+        System.out.println("\n-------- Before Single update --------");
 
         //update
-        update(collection, 9, 14,9,10.0);
+        update(collection, 9, 14, 9, 10.0);
         printAll(collection);
 
 
-
-
-
-
-        // clear the collection while testing
+        //remove documents (all in this case)
         clear(collection);
         System.out.println("\n-------- Collection cleared --------");
+        printAll(collection);
+
 
     }
 
@@ -54,6 +61,8 @@ public class CRUD {
     }
 
     public static void printAll(MongoCollection<Document> collection) {
+        // Print all from query: SELECT * FROM collection
+
         FindIterable<Document> findIterable = collection.find(new Document()); // get all documents from collection
 
         try (MongoCursor<Document> cursor = findIterable.iterator()) {
@@ -62,10 +71,28 @@ public class CRUD {
             }
         }
     }
+
     public static void update(MongoCollection<Document> collection, int oldqty, int newqty, double newH, double newW) {
-        collection.updateOne(eq("qty", oldqty),combine(set("qty", newqty),set("size.h",newH),set("size.w", newW)));
+        collection.updateOne(eq("qty", oldqty), combine(set("qty", newqty), set("size.h", newH), set("size.w", newW)));
     }
+
     public static void clear(MongoCollection<Document> collection) {
         collection.deleteMany(new Document());
+    }
+
+    public static void bulk(MongoCollection<Document> collection) {
+        Document size = new Document("h", 28.0).append("w", 35.5).append("uom", "cm");
+
+        collection.bulkWrite(Arrays.asList(
+                new InsertOneModel<>(new Document("item", "canvas").append("qty", 1).append("tags", singletonList("cotton")).append("size", size)),
+                new InsertOneModel<>(new Document("item", "canvas").append("qty", 2).append("tags", singletonList("cotton")).append("size", size)),
+                new InsertOneModel<>(new Document("item", "canvas").append("qty", 3).append("tags", singletonList("cotton")).append("size", size)),
+                new InsertOneModel<>(new Document("item", "canvas").append("qty", 4).append("tags", singletonList("cotton")).append("size", size)),
+                new InsertOneModel<>(new Document("item", "canvas").append("qty", 5).append("tags", singletonList("cotton")).append("size", size)),
+                new InsertOneModel<>(new Document("item", "canvas").append("qty", 6).append("tags", singletonList("cotton")).append("size", size)),
+                new InsertOneModel<>(new Document("item", "canvas").append("qty", 7).append("tags", singletonList("cotton")).append("size", size)),
+                new InsertOneModel<>(new Document("item", "canvas").append("qty", 8).append("tags", singletonList("cotton")).append("size", size)),
+                new InsertOneModel<>(new Document("item", "canvas").append("qty", 9).append("tags", singletonList("cotton")).append("size", size))
+        ));
     }
 }
